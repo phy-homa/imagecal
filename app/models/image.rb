1aa9acb6fdb85b02a89b72461c5bb76c15d006f1
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-08-02 15:21:18 +0900

    ホームのイラストにタグ表示

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,6 @@ class Image < ApplicationRecord
 
 belongs_to :user
 has_one_attached :picture
-has_many :tags, through: :image_tag_relations
 has_many :image_tag_relations
+has_many :tags, through: :image_tag_relations
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,6 @@ class Tag < ApplicationRecord
 
   validates :name, uniqueness: true
 
-  has_many :images, through: :image_tag_relations
   has_many :image_tag_relations
+  has_many :images, through: :image_tag_relations
 end
