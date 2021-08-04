f61fab8d5d31e015c9cab8159d0be8821c90fbcf
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-08-04 15:04:14 +0900

    設定ページの微調節

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+
   root to: "images#index"
   resources :images, only: [:index, :new, :create, :show]
   resources :users, only: [:new, :show]
