36b389c1afdd4df52ee783b3ff1b5aecb6df9172
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-08-20 16:51:41 +0900

    javascriptエラー解消

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,13 +1,15 @@
 window.addEventListener('load',function(){
-  const pullDownButton = document.getElementById("menutab");
-  const menuList = document.getElementById("menu-list")
-  pullDownButton.addEventListener('click', function(){
-    if (menuList.getAttribute("style") == "display:block;"){
-      pullDownButton.style.opacity = 1;
-      menuList.removeAttribute("style", "display:block;")
-    }else{
-      pullDownButton.style.opacity = 0.7;
-      menuList.setAttribute("style", "display:block;")
-    }
-  })
+  if ( document.getElementById("menutab")){
+    const pullDownButton = document.getElementById("menutab");
+    const menuList = document.getElementById("menu-list")
+    pullDownButton.addEventListener('click', function(){
+      if (menuList.getAttribute("style") == "display:block;"){
+        pullDownButton.style.opacity = 1;
+        menuList.removeAttribute("style", "display:block;")
+      }else{
+        pullDownButton.style.opacity = 0.7;
+        menuList.setAttribute("style", "display:block;")
+      }
+    })
+  }
 })
