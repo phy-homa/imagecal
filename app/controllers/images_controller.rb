17c56ae48283eeee1acb5181aae3d891b4797266
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-08-20 16:03:46 +0900

    タグ切り替え機能の実装

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,6 @@
 class ImagesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :show_monthly, only: :index
 
   def index
     @images = Image.includes(:user).order(created_at: :desc)
@@ -39,5 +40,26 @@ class ImagesController < ApplicationController
   def image_params
     @tag_list = params[:image_tag][:name].split(",")
     params.require(:image_tag).permit(:comment, :picture, :season_id).merge(user_id: current_user.id)
+  end
+
+  def show_monthly
+    @images_jan = Image.show_jan
+    @images_feb = Image.show_feb
+    @images_mar = Image.show_mar
+    @images_apr = Image.show_apr
+    @images_may = Image.show_may
+    @images_jun = Image.show_jun
+    @images_jul = Image.show_jul
+    @images_aug = Image.show_aug
+    @images_sep = Image.show_sep
+    @images_oct = Image.show_oct
+    @images_nov = Image.show_nov
+    @images_dec = Image.show_dec
+    @images_spr = Image.show_spr
+    @images_sum = Image.show_sum
+    @images_aut = Image.show_aut
+    @images_win = Image.show_win
+    @images_all = Image.show_all
+    @images_etc = Image.show_etc
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -11,7 +11,80 @@ class Image < ApplicationRecord
   has_many :image_calendars
   has_many :calendars, through: :image_calendars
 
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :season
+
+  def self.show_jan
+
+    @images_jan = Image.where(season_id: 2 )
+  end
+
+  def self.show_feb
+    @images_feb = Image.where(season_id: 3 )
+  end
+
+  def self.show_mar
+    @images_mar = Image.where(season_id: 4 )
+  end
+
+  def self.show_apr
+    @images_apr = Image.where(season_id: 5 )
+  end
+
+  def self.show_may
+    @images_may = Image.where(season_id: 6 )
+  end
+
+  def self.show_jun
+    @images_jun = Image.where(season_id: 7 )
+  end
+
+  def self.show_jul
+    @images_jul = Image.where(season_id: 8 )
+  end
+
+  def self.show_aug
+    @images_aug = Image.where(season_id: 9 )
+  end
+
+  def self.show_sep
+    @images_sep = Image.where(season_id: 10 )
+  end
+
+  def self.show_oct
+    @images_oct = Image.where(season_id:11)
+  end
+
+  def self.show_nov
+    @images_nov = Image.where(season_id: 12 )
+  end
+
+  def self.show_dec
+    @images_dec = Image.where(season_id: 13)
+  end
+  
+  def self.show_spr
+
+    @images_spr = Image.where(season_id: 14 )
+  end
+
+  def self.show_sum
+    @images_sum = Image.where(season_id: 15 )
+  end
+
+  def self.show_aut
+    @images_aut = Image.where(season_id: 16 )
+  end
+
+  def self.show_win
+    @images_win = Image.where(season_id: 17 )
+  end
+
+  def self.show_all
+    @images_all = Image.where(season_id: 18 )
+  end
+
+  def self.show_etc
+    @images_etc = Image.where(season_id: 19 )
+  end
 end
