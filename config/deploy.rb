0e4be372cdd43f339c12d628dd611a6402d17ca2
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-08-24 11:27:44 +0900

    自動デプロイ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.16.0'
 
 set :application, "imagecal"
-set :repo_url, "https://github.com:phy-homa/imagecal.git"
+set :repo_url, "https://github.com/phy-homa/imagecal.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
