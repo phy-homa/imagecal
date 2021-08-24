72d3ff9fd8f760ab703e7614170203a1164dfb1f
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-08-24 11:25:10 +0900

    自動デプロイ準備

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
+lock '3.16.0'
 
 set :application, "imagecal"
-set :repo_url, "https://github.com/phy-homa/imagecal.git"
+set :repo_url, "https://github.com:phy-homa/imagecal.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server '35.76.155.128', user: 'ec2-user', roles: %w{app db web}
+
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
@@ -59,3 +59,4 @@ server '35.76.155.128', user: 'ec2-user', roles: %w{app db web}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '35.76.155.128', user: 'ec2-user', roles: %w{app db web}
